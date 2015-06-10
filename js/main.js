610e9fa7b4f3454c35e8609a42aa992b7f05e7c9
Dusan Stankovic <dusanstankovic90@gmail.com>
2015-06-10 08:18:54 +0200

    virtual dom implementation

--- js/main.js
+++ js/main.js
@@ -1,34 +1,41 @@
+var mainLoop = require("main-loop"),
+    h = require("virtual-dom/h"),
+    $ = require("jquery"),
+    packery = require("packery"),
+    loop;
+
+
 var widths = [200,400],
     heights = [250,330,380,450,470,480,500,530,550,600],
     images = [],
     image,
-    container = $("#container");
+    width,height,
+    myPack = new Packery('#container');
 
 function loadImages(){
 
   var n = $(location).attr('search').slice(1) || 50;
-
   for(var i=0;i<n;i++){
+      width = widths[ Math.floor(Math.random()*widths.length) ];
+      height = heights[ Math.floor(Math.random()*heights.length) ];
       image = {
-        height: heights[ Math.floor(Math.random()*heights.length) ],
-        width: widths[ Math.floor(Math.random()*widths.length) ]
+        height: height,
+        width: width,
+        src: "http://placekitten.com/g/"+width+"/"+height
       }
       images.push(image);
+      loop.update(images);
+      myPack.reloadItems();
   }
-  _.each(images,appendImage);
 }
 
 function appendImage(image){
-
-  var img = $("<img />");
-  img.attr({
-    src: "http://placekitten.com/g/"+image.width+"/"+image.height,
-    height: image.height,
-    width: image.width,
-    alt: "Image could not be loaded"
-  });
-  container.append(img);
-};
+  return h('img',{
+      src: image.src,
+      width: image.width,
+      height: image.height
+    });
+}
 
 function showScrollToTop() {
 
@@ -45,9 +52,16 @@ function animateScroll(){
     $("html, body").animate({ scrollTop: 0 }, "slow");
 };
 
+function render(state){
+  return h('div#container.packery.js-packery', state.map(appendImage));
+}
+
+  loop = mainLoop(images, render, require('virtual-dom'));
+  document.body.appendChild(loop.target);
+  loadImages();
 
 
-loadImages();
+
 
 $("#top").click(animateScroll);
 $( window ).scroll(showScrollToTop);
